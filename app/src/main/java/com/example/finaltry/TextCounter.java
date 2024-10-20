98ad0c29fca4ecbdef99f145470a196c26802f49
Hamza_salama <60302032+joker9997@users.noreply.github.com>
2024-10-20 17:36:55 +0300

    Update TextCounter.java

--- app/src/main/java/com/example/finaltry/TextCounter.java
+++ app/src/main/java/com/example/finaltry/TextCounter.java
@@ -1,8 +1,5 @@
 package com.example.finaltry;
-
 public class TextCounter {
-
-    // Function to count words in the input
     public int countWords(String input) {
         if (input == null || input.trim().isEmpty()) {
             return 0;
@@ -10,8 +7,6 @@ public class TextCounter {
         String[] words = input.split("[\\s,\\.]+");
         return words.length;
     }
-
-    // Function to count characters in the input
     public int countCharacters(String input) {
         if (input == null || input.isEmpty()) {
             return 0;
